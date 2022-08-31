ecb0d25b3efe350fede9a34b399e9e6e43088f15
Angela Carvalho <angelalopo@outlook.com>
2022-08-31 13:30:31 -0300

    update cors

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
+import cors from 'cors'
 import express from 'express';
 import processos from './processos.json' assert {type: "json"};
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-
+app.use(cors())
 app.use(express.json());
 
 app.listen(PORT,() => {
